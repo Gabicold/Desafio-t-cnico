b282963b536cde8b7057b2a99eb59223eb18610f
Gabicold <biel18102004@gmail.com>
2022-07-10 01:46:02 -0300

    Correçao da componentizaçao da aba equipe

--- src/Components/main/filter/MainFilter.js
+++ src/Components/main/filter/MainFilter.js
@@ -1,233 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import * as S from "./stylesFilter"
-import Photo1 from "./FotosEquipeTécnica/photo1.jpg"
-import Photo2 from "./FotosEquipeTécnica/photo2.jpg"
-import Photo3 from "./FotosEquipeTécnica/photo3.jpg"
-import Photo4 from "./FotosEquipeTécnica/photo4.jpg"
-import Photo5 from "./FotosEquipeTécnica/photo5.jpg"
-import Photo6 from "./FotosEquipeTécnica/photo6.jpg"
-import Photo7 from "./FotosEquipeTécnica/photo7.jpg"
-import Photo8 from "./FotosEquipeTécnica/photo8.jpg"
-import Photo9 from "./FotosEquipeTécnica/photo9.jpg"
-import Photo10 from "./FotosEquipeTécnica/photo10.jpg"
-import Photo11 from "./FotosEquipeTécnica/photo11.jpg"
-import Photo12 from "./FotosEquipeTécnica/photo12.jpg"
-import Photo13 from "./FotosEquipeTécnica/photo13.jpg"
-import Photo14 from "./FotosEquipeTécnica/photo14.jpg"
-import Photo15 from "./FotosEquipeTécnica/photo15.jpg"
-/*
-export default class App extends React.Component{
+import Team from "./Team"
 
-    state = [
-        {
-            name:"Dimitri Duque",
-            fuction:"#Voluntários",
-            image:`${Photo2}`
-        },
-        {
-            name:"Hugo Sabino",
-            fuction:"#Voluntários",
-            image:`${Photo6}`
-        },
-        {
-            name:"Karla De Melo",
-            fuction:"#Voluntários",
-            image:`${Photo10}`
-        },
-        {
-            name:"Paula Misan",
-            fuction:"#Voluntários",
-            image:`${Photo4}`
-        },
-        {
-            name:"Thais Brait",
-            fuction:"#Voluntários",
-            image:`${Photo12}`
-        },
-        {
-            name:"Aline Fróes",
-            fuction:"#CEO",
-            image:`${Photo1}`
-        },
-        {
-            name:"Aline Nogueira",
-            fuction:"#Agente Socioemocional",
-            image:`${Photo5}`
-        },
-        {
-            name:"Andrea Marino",
-            fuction:"#Pisicóloga",
-            image:`${Photo9}`
-        },
-        {
-            name:"Cris Dos Prazeres",
-            fuction:"#Coordenadorea Executiva",
-            image:`${Photo13}`
-        },
-        {
-            name:"Karynne Moreira",
-            fuction:"#Facilitadora Técnica",
-            image:`${Photo14}`
-        },
-        {
-            name:"Lizia Merlim",
-            fuction:"#Agente Socioemocional",
-            image:`${Photo3}`
-        },
-        {
-            name:"Marina Correia",
-            fuction:"#Coordenadorea Administrativa",
-            image:`${Photo7}`
-        },
-        {
-            name:"Marina Tiago",
-            fuction:"#Agente de Estratégia",
-            image:`${Photo11}`
-        },
-        {
-            name:"Marlon Yuri",
-            fuction:"#Instrutor Técnico",
-            image:`${Photo15}`
-        },
-        {
-            name:"Priscila Tufani",
-            fuction:"#Agente de Estratégia",
-            image:`${Photo8}`
-        },
-    ]
-
-    handleChange = (e) =>{
-        
-        
-    }
-
-    render(){
-        return(
-            <S.BoxEquipe>
-                <S.Equipe>Equipe</S.Equipe>
-                <S.BoxInput>
-                        <S.TeamInput
-                        type="text" 
-                        onChange={this.handleChange}
-                        placeholder="Pesquisar...">
-                        </S.TeamInput>
-                </S.BoxInput>
-                <S.BoxTeam>
-                    {this.state.map(item => (
-                        <S.BoxImage>
-                            <S.TeamImage src={item.image}/>
-                            <S.ImageName>{item.name}</S.ImageName>
-                            <S.TextFunction>{item.fuction}</S.TextFunction>
-                            
-                        </S.BoxImage>
-                    ))}
-                </S.BoxTeam>
-                
-            </S.BoxEquipe>
-        )
-    }
-}
-*/
 export default function MainFilter(){
 
-    const [team] = useState([
-        {
-            name:"Dimitri Duque",
-            fuction:"#Voluntários",
-            image:`${Photo2}`
-        },
-        {
-            name:"Hugo Sabino",
-            fuction:"#Voluntários",
-            image:`${Photo6}`
-        },
-        {
-            name:"Karla De Melo",
-            fuction:"#Voluntários",
-            image:`${Photo10}`
-        },
-        {
-            name:"Paula Misan",
-            fuction:"#Voluntários",
-            image:`${Photo4}`
-        },
-        {
-            name:"Thais Brait",
-            fuction:"#Voluntários",
-            image:`${Photo12}`
-        },
-        {
-            name:"Aline Fróes",
-            fuction:"#CEO",
-            image:`${Photo1}`
-        },
-        {
-            name:"Aline Nogueira",
-            fuction:"#Agente Socioemocional",
-            image:`${Photo5}`
-        },
-        {
-            name:"Andrea Marino",
-            fuction:"#Pisicóloga",
-            image:`${Photo9}`
-        },
-        {
-            name:"Cris Dos Prazeres",
-            fuction:"#Coordenadorea Executiva",
-            image:`${Photo13}`
-        },
-        {
-            name:"Karynne Moreira",
-            fuction:"#Facilitadora Técnica",
-            image:`${Photo14}`
-        },
-        {
-            name:"Lizia Merlim",
-            fuction:"#Agente Socioemocional",
-            image:`${Photo3}`
-        },
-        {
-            name:"Marina Correia",
-            fuction:"#Coordenadorea Administrativa",
-            image:`${Photo7}`
-        },
-        {
-            name:"Marina Tiago",
-            fuction:"#Agente de Estratégia",
-            image:`${Photo11}`
-        },
-        {
-            name:"Marlon Yuri",
-            fuction:"#Instrutor Técnico",
-            image:`${Photo15}`
-        },
-        {
-            name:"Priscila Tufani",
-            fuction:"#Agente de Estratégia",
-            image:`${Photo8}`
-        },
-      ])
+    
       const [teamFilter, setTeamFilter] = useState([])
 
   useEffect(()=>{
-    setTeamFilter(team)
-  }, [team])
+    setTeamFilter(Team)
+  }, [])
  
     return(
         <S.BoxEquipe>
             <S.Equipe>Equipe</S.Equipe>
             <S.BoxInput>
-            <S.TeamInput type="text" placeholder="Pesquisar..." onChange={(event)=>{
+            <S.TeamInput type="text" placeholder="Pesquisar..." onChange={(e)=>{
             
-            if(event.target.value !== "" || null){
-              const NamesFilter = team.filter(item => {
-              if(item.name.toLowerCase().includes(event.target.value.toLowerCase())){
-                return true;
-              }
+            if(e.target.value !== "" || null){
+              const NamesFilter = teamFilter.filter(item => {
+              return item.name.toLowerCase().includes(e.target.value.toLowerCase())
             })
             setTeamFilter(NamesFilter)
           }else{
-            setTeamFilter(team)
+            setTeamFilter(Team)
           }
           }}/>
             </S.BoxInput>
